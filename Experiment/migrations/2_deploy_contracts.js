2c2580201cf563d0090286b747198cd8723e1e0f
Roy Shadmon <royshadmon@eduroam-169-233-251-78.ucsc.edu>
2019-02-05 18:22:02 -0800

    create/join work

--- Experiment/migrations/2_deploy_contracts.js
+++ Experiment/migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
 const ConvertLib = artifacts.require("ConvertLib");
 const MetaCoin = artifacts.require("MetaCoin");
+const twentyOne = artifacts.require('TwentyOneGame')
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
+  //deployer.deploy(twentyOne);
 };

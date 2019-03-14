47c66800f4a7be4eee3d842e2cf33e65e2ad9b98
Roy Shadmon <royshadmon@eduroam-169-233-251-78.ucsc.edu>
2019-03-14 10:33:48 -0700

    can't see dapp unless metamask is enabled

--- Experiment/app/src/index.js
+++ Experiment/app/src/index.js
@@ -813,20 +813,25 @@ window.addEventListener("load", async function() {
   if (window.ethereum) {
     // use MetaMask's provider
     App.web3 = new Web3(window.ethereum);
-    window.ethereum.enable(); // get permission to access accounts
+    document.getElementById('noMask').style.display='block';
+    await window.ethereum.enable(); // get permission to access accounts
+    document.getElementById('noMask').style.display='none';
+    document.getElementById('functionality').style.display='block';
+    App.start();
   } 
-  // else {
-  //   console.warn(
-  //     "No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live",
-  //   );
-  //   // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-  //   web3 = new Web3(
-  //     new Web3.providers.HttpProvider("http://127.0.0.1:9545"),
-  //   );
-  // }
+  else {
+    document.getElementById('noMask').style.display='block';
+    console.warn(
+      "No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live",
+    );
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    // web3 = new Web3(
+    //   new Web3.providers.HttpProvider("http://127.0.0.1:9545"),
+    // );
+  }
 
   // accounts = await web3.eth.getAccounts();
   // account = accounts[0];
   // console.log("ACCOUNT2", account);
-  App.start();
+  
 });
